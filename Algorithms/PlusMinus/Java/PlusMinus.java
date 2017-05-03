cdcd619d3e1f46f9404290a6e0337bd847ab04db
Boris López <borosilva@gmail.com>
2017-05-03 15:12:21 -0300

    cambio en explicación

--- Algorithms/PlusMinus/Java/PlusMinus.java
+++ Algorithms/PlusMinus/Java/PlusMinus.java
@@ -39,7 +39,6 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-
     public static void main(String[] args) {
         Scanner in      = new Scanner(System.in);
         int largo       = in.nextInt();
