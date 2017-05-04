5c99821b27acaf1dd52eb1bb0d49c0e559b9c07f
Boris López <borosilva@gmail.com>
2017-05-04 12:27:15 -0300

    se agrega solución para Staircase

---
+++ Algorithms/Staircase/Java/Staircase.java
@@ -0,0 +1,67 @@
+/*
+Consider a staircase of size n = 4:
+
+   #
+  ##
+ ###
+####
+
+Observe that its base and height are both equal to n, and the image is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
+Write a program that prints a staircase of size n.
+
+Input Format
+
+A single integer, n, denoting the size of the staircase.
+
+Output Format
+
+Print a staircase of size n using # symbols and spaces.
+
+Note: The last line must have 0 spaces in it.
+
+Sample Input
+
+6 
+
+Sample Output
+
+     #
+    ##
+   ###
+  ####
+ #####
+######
+
+Explanation
+
+The staircase is right-aligned, composed of # symbols and spaces, and has a height and width of n = 6.*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in      = new Scanner(System.in);
+        int largo       = in.nextInt();
+
+        for(int i = 1; i <= largo; i++){
+            int blancos = largo-i;
+            int gatos   = i;
+            
+            if(blancos > 0){
+                for(int y = 0; y < blancos; y++){
+                    System.out.print(" ");
+                }
+            }
+            
+            for(int x = 0; x < gatos; x++){
+                System.out.print("#");
+            }
+            
+            System.out.print("\n");
+        }
+
+    }
+}
