647dfbc19fa673e2ddde78b53ae2d946c923f0bd
Boris López <borosilva@gmail.com>
2017-05-15 12:17:56 -0400

    se agrega solución

---
+++ SQL/BasicSelect/RevisingTheSelectQueryII.sql
@@ -0,0 +1,21 @@
+/*
+Query the names of all American cities in CITY with populations larger than 120000. The CountryCode for 
+America is USA.
+
+Input Format
+
+The CITY table is described as follows:
+
+---------------------------------------
+*               CITY                  *
+---------------------------------------
+|     Field       |         Type      |
+---------------------------------------
+|  ID             |     NUMBER        |
+|  NAME           |     VARCHAR2(17)  |
+|  COUNTRYCODE    |     VARCHAR2(3)   |
+|  DISTRICT       |     VARCHAR2(20)  |
+|  POPULATION     |     NUMBER        |
+---------------------------------------     
+*/
+select NAME from CITY where COUNTRYCODE = 'USA' and POPULATION > 120000;
